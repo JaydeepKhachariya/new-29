337ea65415db8c0efeedb2e02470e0dbe11c4b95
Jaydeep <khachariyajaydeep@gmail.com>
2023-06-29 00:05:47 +0530

    Make siderbar

--- src/pages/Plans.jsx
+++ src/pages/Plans.jsx
@@ -7,9 +7,12 @@ import Checkout from "../components/Checkout";
 import "animate.css";
 
 const Plan = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const [isClicked, setIsClicked] = useState(false);
+  const [selectedPlan, setSelectedPlan] = useState(null);
+  const [isSelected, setIsSelected] = useState();
+  const [showModel, setShowModel] = useState(false);
 
   const arr = [
     {
@@ -29,9 +32,45 @@ const Plan = () => {
     },
   ];
 
-  const [isSelected, setIsSelected] = useState();
-  const [showModel, setShowModel] = useState(false);
-  console.log(isSelected);
+  const plans = [
+    {
+      id: 1,
+      name: "Beginner",
+      image: beginner,
+      rad: 1,
+      dad: 1,
+      chingari: 1,
+      wabisabi: 1,
+      workers: 4,
+      mother: 1,
+    },
+    {
+      id: 2,
+      name: "Intermediate",
+      image: intermediet,
+      rad: 1,
+      dad: 1,
+      chingari: 1,
+      wabisabi: 1,
+      workers: 8,
+      mother: 1,
+    },
+    {
+      id: 3,
+      name: "Advance",
+      image: expert,
+      rad: 1,
+      dad: 1,
+      chingari: 1,
+      wabisabi: 1,
+      workers: 16,
+      mother: 1,
+    },
+  ];
+
+  const handlePlanClick = (plan) => {
+    setSelectedPlan(plan);
+  };
 
   const handleClick = (id) => {
     setIsClicked(id);
@@ -45,13 +84,14 @@ const Plan = () => {
         <div className="h-[100%] top-0 relative">
           {isSelected == undefined ? (
             <div className="flex items-center justify-around  h-[100%]">
-              {arr.map((el) => {
+              {plans.map((el) => {
                 return (
                   <div
                     key={el.id}
                     className="flex flex-col items-center justify-center p-4 h-[250px] w-[250px]"
                     onClick={() => {
                       handleClick(el.id);
+                      handlePlanClick(el);
                     }}
                   >
                     <img
@@ -75,7 +115,9 @@ const Plan = () => {
                       <div className="flip-card-front">
                         <img src="rad.png" alt="" />
                       </div>
-                      <div className="flip-card-back">2</div>
+                      <div className="flip-card-back">
+                        RAD {selectedPlan.rad}$
+                      </div>
                     </div>
                   </div>
                   <div className="h-[150px] w-[150px] rounded-full bg-transparent flip-card">
@@ -83,7 +125,9 @@ const Plan = () => {
                       <div className="flip-card-front">
                         <img src="mother.png" alt="" className="w-[130px]" />
                       </div>
-                      <div className="flip-card-back">2</div>
+                      <div className="flip-card-back">
+                        Mother {selectedPlan.mother}$
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -93,25 +137,31 @@ const Plan = () => {
                       <div className="flip-card-front">
                         <img src="chingari.png" alt="" className="w-[100px]" />
                       </div>
-                      <div className="flip-card-back">Chingari</div>
+                      <div className="flip-card-back">
+                        Chingari {selectedPlan.chingari}$
+                      </div>
                     </div>
                   </div>
                   <div className="h-[150px] w-[150px] rounded-full bg-transparent flip-card">
                     <div className="flip-card-inner">
                       <div className="flip-card-front">
                         <img
-                          src={
-                            isSelected === 1
-                              ? beginner
-                              : isSelected === 2
-                              ? intermediet
-                              : expert
-                          }
+                          src={selectedPlan.image}
+                          // src={
+                          //   isSelected === 1
+                          //     ? beginner
+                          //     : isSelected === 2
+                          //     ? intermediet
+                          //     : expert
+                          // }
                           alt=""
                           className="w-[100px] animate__animated animate__slideInLeft animate__faster"
                         />
                       </div>
-                      <div className="flip-card-back">
+                      <div
+                        className="flip-card-back"
+                        onClick={() => setIsOpen(true)}
+                      >
                         <button>Book now</button>
                       </div>
                     </div>
@@ -121,7 +171,9 @@ const Plan = () => {
                       <div className="flip-card-front ">
                         <img src="teamwork2.png" alt="" className="w-[90px]" />
                       </div>
-                      <div className="flip-card-back">2</div>
+                      <div className="flip-card-back">
+                        Workers {selectedPlan.workers}$
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -131,7 +183,9 @@ const Plan = () => {
                       <div className="flip-card-front">
                         <img src="wabiSabi.png" alt="" className="w-[120px]" />
                       </div>
-                      <div className="flip-card-back">2</div>
+                      <div className="flip-card-back">
+                        Wabisabi {selectedPlan.wabisabi}$
+                      </div>
                     </div>
                   </div>
                   <div className="h-[150px] w-[150px] rounded-full bg-transparent flip-card">
@@ -139,20 +193,22 @@ const Plan = () => {
                       <div className="flip-card-front">
                         <img src="dad.png" alt="" />
                       </div>
-                      <div className="flip-card-back">2</div>
+                      <div className="flip-card-back">
+                        DAD {selectedPlan.dad}$
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
 
               <div className="mt-10 w-3/4 mx-auto flex justify-evenly gap-4 items-center   ">
-                {arr.map((el) => {
+                {plans.map((el) => {
                   return (
                     <div
                       key={el.id}
-                      onClick={() => setIsSelected(el.id)}
+                      onClick={() => handlePlanClick(el)}
                       className={`${
-                        isSelected === el.id
+                        selectedPlan.id === el.id
                           ? " bg-blue-400 text-white"
                           : "bg-[white] shadow-xl"
                       } flex flex-col items-center justify-center p-4 rounded-2xl h-[50px] w-[150px]`}
@@ -169,7 +225,7 @@ const Plan = () => {
       </div>
       {/* <div className=""> */}
       {isOpen ? (
-        <div className="bg-white flex flex-col p-4  shadow-xl w-[20%] my-8 mx-2 h-[90vh] rounded-2xl absolute top-0 right-0">
+        <div className=" bg-white flex flex-col p-4  shadow-xl w-[20%] my-8 mx-2 h-[90vh] rounded-2xl absolute top-0 right-0">
           <div className="flex items-center justify-between w-full">
             <p className="text-[32px]">Cart Items</p>
             <MdClose
@@ -179,37 +235,84 @@ const Plan = () => {
             />
           </div>
 
-          <div className="bg-white cartShadow rounded-xl flex flex-col mt-2 gap-4 p-4">
-            <div className="h-[70px] flex items-center w-full bg-[#60a5fa] py-1 px-2 rounded-xl">
-              <div className="flex gap-2 items-start">
-                <div className="flex items-start gap-2"><div className="h-[50px] w-[50px] rounded-md bg-white"></div>
-                <div className="flex flex-col items-start">
-                  <p className="text-[20px] text-white uppercase">Rad</p>
-                  <p className=" text-gray-200 text-[16px] ">Price : $200</p>
-                </div></div>
-                
+          <div className="bg-white cartShadow rounded-xl flex flex-col mt-2 gap-4 p-2">
+            <div className="h-[50px] flex items-center w-full bg-[#60a5fa] py-1 px-2 rounded-lg">
+              <div className="flex w-full justify-between pr-2 items-center">
+                <div className="flex items-center gap-2">
+                  <div className="flex flex-col items-start">
+                    <p className="text-[16px] text-white uppercase">DAD</p>
+                    <p className=" text-gray-200 text-[14px] ">
+                      Price : ${selectedPlan.dad}
+                    </p>
+                  </div>
+                </div>
                 <p>1X</p>
               </div>
             </div>
-            <div className="h-[70px] flex items-center w-full bg-[#60a5fa] py-1 px-2 rounded-xl">
-              <div className="flex gap-2 items-start">
-                <div className="flex items-start gap-2"><div className="h-[50px] w-[50px] rounded-md bg-white"></div>
-                <div className="flex flex-col items-start">
-                  <p className="text-[20px] text-white uppercase">Rad</p>
-                  <p className=" text-gray-200 text-[16px] ">Price : $200</p>
-                </div></div>
-                
+
+            <div className="h-[50px] flex items-center w-full bg-[#60a5fa] py-1 px-2 rounded-lg">
+              <div className="flex w-full justify-between pr-2 items-center">
+                <div className="flex items-center gap-2">
+                  <div className="flex flex-col items-start">
+                    <p className="text-[16px] text-white uppercase">RAD</p>
+                    <p className=" text-gray-200 text-[14px] ">
+                      Price : ${selectedPlan.rad}
+                    </p>
+                  </div>
+                </div>
+
                 <p>1X</p>
               </div>
             </div>
-            <div className="h-[70px] flex items-center w-full bg-[#60a5fa] py-1 px-2 rounded-xl">
-              <div className="flex gap-2 items-start">
-                <div className="flex items-start gap-2"><div className="h-[50px] w-[50px] rounded-md bg-white"></div>
-                <div className="flex flex-col items-start">
-                  <p className="text-[20px] text-white uppercase">Rad</p>
-                  <p className=" text-gray-200 text-[16px] ">Price : $200</p>
-                </div></div>
-                
+            <div className="h-[50px] flex items-center w-full bg-[#60a5fa] py-1 px-2 rounded-lg">
+              <div className="flex w-full justify-between pr-2 items-center">
+                <div className="flex items-center gap-2">
+                  <div className="flex flex-col items-start">
+                    <p className="text-[16px] text-white uppercase">Chingari</p>
+                    <p className=" text-gray-200 text-[14px] ">
+                      Price : ${selectedPlan.chingari}
+                    </p>
+                  </div>
+                </div>
+                <p>1X</p>
+              </div>
+            </div>
+            <div className="h-[50px] flex items-center w-full bg-[#60a5fa] py-1 px-2 rounded-lg">
+              <div className="flex w-full justify-between pr-2 items-center">
+                <div className="flex items-center gap-2">
+                  <div className="flex flex-col items-start">
+                    <p className="text-[16px] text-white uppercase">Mother</p>
+                    <p className=" text-gray-200 text-[14px] ">
+                      Price : ${selectedPlan.mother}
+                    </p>
+                  </div>
+                </div>
+                <p>1X</p>
+              </div>
+            </div>
+            <div className="h-[50px] flex items-center w-full bg-[#60a5fa] py-1 px-2 rounded-lg">
+              <div className="flex w-full justify-between pr-2 items-center">
+                <div className="flex items-center gap-2">
+                  <div className="flex flex-col items-start">
+                    <p className="text-[16px] text-white uppercase">Wabisabi</p>
+                    <p className=" text-gray-200 text-[14px] ">
+                      Price : ${selectedPlan.wabisabi}
+                    </p>
+                  </div>
+                </div>
+                <p>1X</p>
+              </div>
+            </div>
+            <div className="h-[50px] flex items-center w-full bg-[#60a5fa] py-1 px-2 rounded-lg">
+              <div className="flex w-full justify-between pr-2 items-center">
+                <div className="flex items-center gap-2">
+                  <div className="flex flex-col items-start">
+                    <p className="text-[16px] text-white uppercase">Workers</p>
+                    <p className=" text-gray-200 text-[14px] ">
+                      Price : ${selectedPlan.workers}
+                    </p>
+                  </div>
+                </div>
                 <p>1X</p>
               </div>
             </div>
